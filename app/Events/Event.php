0e417ecde6f26cd9940b28b8826196037d73a4c3
andela-cijeomah <chidozie.ijeomah@andela.com>
2015-07-14 11:56:42 +0100

    Installed the Laravel 5 base framework

---
+++ app/Events/Event.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace ChopBox\Events;
+
+abstract class Event
+{
+    //
+}
---
+++ app/Http/Requests/Request.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ChopBox\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+abstract class Request extends FormRequest
+{
+    //
+}
