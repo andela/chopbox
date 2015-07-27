5ebd0c7154df0c77af9f46d34d4d93e65f48f393
vdugeri <verem.dugeri@andela.co>
2015-07-27 15:49:48 +0100

    add separate migration for columns in users table

---
+++ database/migrations/2015_07_27_144250_add_columns_to_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnsToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('firstname')->nullable();
+            $table->string('lastname')->nullable();
+            $table->string('location')->nullable();
+            $table->string('best_food')->nullable();
+            $table->string('about')->nullable();
+            $table->string('gender')->nullable();
+            $table->boolean('status')->nullable();
+            $table->string('image_uri')->nullable();
+            $table->boolean('profile_state');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
