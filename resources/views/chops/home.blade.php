f3b3251c77e6df27cf8c301cacca6bef74542bcc
vdugeri <verem.dugeri@andela.co>
2015-08-10 12:28:32 +0100

    Merging changes

--- resources/views/chops/home.blade.php
+++ resources/views/chops/home.blade.php
@@ -8,8 +8,17 @@
 @section('content')
         @foreach($chops as $chop)
             <div>
+<<<<<<< Updated upstream
                 <h1>{{$chop->name}}</h1>
                 <img src="#" alt="chops image">
+=======
+                <div><h2>{{$chop->chops_name}}</h2></div>
+                @foreach($chop->uploads as $image)
+                    <div>
+                        <img src="{{$image->file_uri}}" alt="chops image">
+                    </div>
+                @endforeach
+>>>>>>> Stashed changes
                 <p>
                     {{$chop->about}}
                 </p>
