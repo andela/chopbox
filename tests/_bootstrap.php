4b046efcc1e695dcbbe59f6d7bc926ed3f1ac1e5
busayo <prosperotemuyiwa@gmail.com>
2015-08-16 11:48:53 +0100

    Add DB config from codeception.yml to _bootstrap.php for global autoloading

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,2 +1,12 @@
 <?php
 // This is global bootstrap for autoloading
+\Codeception\Configuration::$defaultSuiteSettings['modules']['config'] = [
+    'Db' => [
+        'dsn' => 'mysql:host=' . getenv('DB_URL') . ';dbname=' . getenv('DB_NAME'),
+        'user' => getenv('DB_USER'),
+        'password' => getenv(''),
+        'dump' => '_data/dump.sql',
+        'populate' => true,
+        'cleanup' => true
+    ]
+];
