4161229529c7ae190a29abaa3bf01415ae0de12b
andela-kerinoso <kolawole.erinoso@andela.com>
2015-08-20 09:23:03 +0000

    Add a column for cloudinary public_id to table uploads

---
+++ database/migrations/2015_08_19_172930_AddCLoudinaryPublicIdColumn.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCLoudinaryPublicIdColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('uploads', function (Blueprint $table) {
+            $table->string('public_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('uploads', function (Blueprint $table) {
+            $table->dropColumn('public_id');
+        });
+    }
+}
