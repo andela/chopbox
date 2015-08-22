214704569a018c8f6e6fca1cb3cdfa36da841939
andela-kerinoso <kolawole.erinoso@andela.com>
2015-08-22 11:28:14 +0000

    Uncompleted test

--- tests/functional/CreateChopsCept.php
+++ tests/functional/CreateChopsCept.php
@@ -1,21 +1,24 @@
-<?php 
+<?php
 $I = new FunctionalTester($scenario);
-$I->expectTo('have a usere in the database');
+$I->expectTo('have a user in the database');
 $I->haveRecord('users', [
-    'username'=>'verem',
+    'username'=>'Dugeri',
     'password' => 'danverem',
-    'email' => 'danverem@gmail.com',
+    'email' => 'verem.dugeri@gmail.com',
     'profile_state' => 0
 ]);
 $I->expectTo('have a logged in user');
-$user = $I->grabRecord('users', ['username'=>'verem']);
-$I->amLoggedAs(['username'=> $user->username, 'id' => $user->id, 'password' => $user->password]);
+$user = $I->grabRecord('users', ['username'=>'Dugeri']);
+//$I->dontSee('Login');
+$I->amHttpAuthenticated('kola', 'secret');
+//$I->amHttpAuthenticated($user->username, $user->password);
+//$I->amLoggedAs(['username'=> $user->username, 'id' => $user->id, 'password' => $user->password]);
+
 $I->wantTo('test if chops is posting to database');
 $I->amOnAction('ChopsController@create');
-$I->seeInCurrentUrl('/create');
-$I->see('What\'s that special meal you just ate today');
-$I->fillField('name', 'edikaikong');
-$I->attachFile('image', 'julia.jpeg');
+//$I->seeInCurrentUrl('/create');
+
 $I->fillField('about','This food is the best dish in the country');
-$I->click('submitButton');
-$I->seeInCurrentUrl('/chops');
+$I->attachFile('image[]', 'dodo.jpg', 'hamburger.jpg', 'ijekuje.jpg');
+$I->click('Post');
+//$I->seeInCurrentUrl('/chops');
