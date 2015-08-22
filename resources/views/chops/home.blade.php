0dcc9d5118fe67644a92490744a49afe24a578da
andela-kerinoso <kolawole.erinoso@andela.com>
2015-08-22 11:47:03 +0000

    Merge changes

--- resources/views/chops/home.blade.php
+++ resources/views/chops/home.blade.php
@@ -6,6 +6,34 @@
 
 
 @section('content')
+    <div class="container">
+
+        {!! Form::open(['url' => 'chops', 'files' => true, 'method'=>'post']) !!}
+        <div class="form-group">
+            {!! Form::textarea('about', null, ['class' => 'form-control', 'placeholder' => "What's that special meal you just ate today", 'required' => 'required']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('image', 'How does it look like?') !!}
+            {!! Form::file('image[]', ['multiple'=> true, 'required' => 'required']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::submit('Post', ['class' =>'btn btn-primary', 'name' =>'submitButton']) !!}
+        </div>
+        {!! Form::close() !!}
+
+        @if($errors->any())
+            <div class="has-error">
+                <ul>
+                    @foreach($errors->all() as $error)
+                        <li>{{$error}}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+    </div>
         @foreach($chops as $chop)
             <div>
                 <div><h2>{{$chop->chops_name}}</h2></div>
