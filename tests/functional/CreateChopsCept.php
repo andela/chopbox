d23e91d174b46c05c41ad03aae388dea30e0f5c7
andela-kerinoso <kolawole.erinoso@andela.com>
2015-08-23 09:19:06 +0000

    Partial functional test for multiple images upload functionality

--- tests/functional/CreateChopsCept.php
+++ tests/functional/CreateChopsCept.php
@@ -1,24 +1,38 @@
 <?php
 $I = new FunctionalTester($scenario);
-$I->expectTo('have a user in the database');
-$I->haveRecord('users', [
+//$I->expectTo('have a user in the database');
+$id = $I->haveRecord('users', [
     'username'=>'Dugeri',
     'password' => 'danverem',
     'email' => 'verem.dugeri@gmail.com',
-    'profile_state' => 0
+    'profile_state' => 1
 ]);
-$I->expectTo('have a logged in user');
+//$I->expectTo('have a logged in user');
 $user = $I->grabRecord('users', ['username'=>'Dugeri']);
-//$I->dontSee('Login');
-$I->amHttpAuthenticated('kola', 'secret');
-//$I->amHttpAuthenticated($user->username, $user->password);
-//$I->amLoggedAs(['username'=> $user->username, 'id' => $user->id, 'password' => $user->password]);
-
+////$I->dontSee('Login');
+$I->amHttpAuthenticated('kola', $user->password);
+$user = \ChopBox\User::find($id);
+$I->amLoggedAs($user);
 $I->wantTo('test if chops is posting to database');
-$I->amOnAction('ChopsController@create');
+$I->amOnAction('ChopsController@index');
+$I->dontSeeRecord('uploads', array('name' => 'suya.jpeg'));
+$I->dontSeeRecord('uploads', array('name' => 'hamburger.jpg'));
+$I->dontSeeRecord('chops', array('about' => 'This food is the best dish in the country'));
 //$I->seeInCurrentUrl('/create');
+$I->submitForm('#my-form', [
+    'about' => 'This food is the best dish in the country',
+    'image' => [
+        'suya.jpeg',
+        'hamburger.jpg',
+        'ijekuje.jpg',
+        'flower.jpg',
+        'food.jpg']
+    ]);
+$I->seeRecord('chops', array('about' => 'This food is the best dish in the country'));
+$I->seeRecord('uploads', array('name' => 'suya.jpeg'));
+$I->seeRecord('uploads', array('name' => 'hamburger.jpg'));
 
-$I->fillField('about','This food is the best dish in the country');
-$I->attachFile('image[]', 'dodo.jpg', 'hamburger.jpg', 'ijekuje.jpg');
-$I->click('Post');
+//$I->fillField('about','This food is the best dish in the country');
+//$I->attachFile('image', ['dodo.jpg', 'hamburger.jpg', 'ijekuje.jpg']);
+//$I->click('Post');
 //$I->seeInCurrentUrl('/chops');
