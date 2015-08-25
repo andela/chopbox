1eb51be0cde8bb165983ba038ce4c689779f8d91
andela-kerinoso <kolawole.erinoso@andela.com>
2015-08-25 10:01:24 +0000

    Complete functional test for single image upload for a chop

--- tests/functional/CreateChopsCept.php
+++ tests/functional/CreateChopsCept.php
@@ -1,38 +1,28 @@
 <?php
 $I = new FunctionalTester($scenario);
-//$I->expectTo('have a user in the database');
+$I->expectTo('have a user in the database');
 $id = $I->haveRecord('users', [
     'username'=>'Dugeri',
     'password' => 'danverem',
     'email' => 'verem.dugeri@gmail.com',
-    'profile_state' => 1
+    'profile_state' => 0
 ]);
-//$I->expectTo('have a logged in user');
-$user = $I->grabRecord('users', ['username'=>'Dugeri']);
-////$I->dontSee('Login');
-$I->amHttpAuthenticated('kola', $user->password);
+
 $user = \ChopBox\User::find($id);
 $I->amLoggedAs($user);
+
 $I->wantTo('test if chops is posting to database');
 $I->amOnAction('ChopsController@index');
-$I->dontSeeRecord('uploads', array('name' => 'suya.jpeg'));
-$I->dontSeeRecord('uploads', array('name' => 'hamburger.jpg'));
-$I->dontSeeRecord('chops', array('about' => 'This food is the best dish in the country'));
-//$I->seeInCurrentUrl('/create');
-$I->submitForm('#my-form', [
-    'about' => 'This food is the best dish in the country',
-    'image' => [
-        'suya.jpeg',
-        'hamburger.jpg',
-        'ijekuje.jpg',
-        'flower.jpg',
-        'food.jpg']
-    ]);
-$I->seeRecord('chops', array('about' => 'This food is the best dish in the country'));
-$I->seeRecord('uploads', array('name' => 'suya.jpeg'));
-$I->seeRecord('uploads', array('name' => 'hamburger.jpg'));
+$I->dontSeeRecord('uploads', ['name' => 'suya.jpeg']);
+$I->dontSeeRecord('uploads', ['name' => 'hamburger.jpg']);
+$I->dontSeeRecord('chops', ['about' => 'This food is the best dish in the country']);
 
-//$I->fillField('about','This food is the best dish in the country');
-//$I->attachFile('image', ['dodo.jpg', 'hamburger.jpg', 'ijekuje.jpg']);
-//$I->click('Post');
-//$I->seeInCurrentUrl('/chops');
+
+$I->fillField('about','This food is the best dish in the country');
+$I->attachFile('image[]', 'suya.jpeg', 'hamburger.jpg');
+
+$I->click('Post');
+
+$I->seeRecord('chops', ['about' => 'This food is the best dish in the country']);
+$I->seeRecord('uploads', ['name' => 'suya.jpeg']);
+
