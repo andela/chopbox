77868fceda29dda0719f3c3e67fa883da979104c
andela-kerinoso <kolawole.erinoso@andela.com>
2015-08-25 10:08:04 +0000

    Include necessary configurations for testing

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,2 +1,12 @@
 <?php
 // This is global bootstrap for autoloading
+\Codeception\Configuration::$defaultSuiteSettings['modules']['config'] = [
+    'Db' => [
+        'dsn' => 'mysql:host=' . getenv('DB_URL') . ';dbname=' . getenv('DB_NAME'),
+        'user' => getenv('DB_USER'),
+        'password' => '',
+        'dump' => 'tests/_data/dump.sql',
+        'populate' => true,
+        'cleanup' => true
+    ]
+];
