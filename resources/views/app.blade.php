3f87d7fa12baa6931d418609b1ff612fce7214e4
andela-kerinoso <kolawole.erinoso@andela.com>
2015-08-26 09:28:23 +0000

    Remove dropzone.css link

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,7 +11,6 @@
 <!-- Custom CSS -->
 <link href="{!! asset('css/landing-page.css') !!}" rel="stylesheet">
     <link href="{!! asset('css/forms.css') !!}" rel="stylesheet" type="text/css">
-    <link href="{!! asset('css/dropzone.css') !!}" rel="stylesheet" type="text/css" media="all">
 
 <!-- Custom Fonts -->
 <link href="{!! asset('font-awesome/css/font-awesome.min.css') !!}" rel="stylesheet" type="text/css">
