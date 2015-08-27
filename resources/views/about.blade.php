5994561a58efabd3c2e38419706e37fd299bc3a9
andela-kerinoso <kolawole.erinoso@andela.com>
2015-08-27 14:55:14 +0000

    Newly created

---
+++ resources/views/about.blade.php
@@ -0,0 +1,151 @@
+<!doctype html>
+<html>
+<head>
+  <!-- General Head Content -->
+  <!-- meta tags -->
+<meta charset="utf-8" />
+<!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1' /><![endif]-->
+
+<meta name="description" content="Introducing Official Bootstrap themes from the founders of Bootstrap. In the last four years we’ve spent thousands of hours designing and developing Bootstrap a" />
+
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta name="google-site-verification" content="4--Yv8-XVyrl4hAM2ceeBXt8H8iiI_3Ms3vDSIV5xN0" />
+
+
+<!-- title tag-->
+<title>
+  About
+</title>
+
+    <!-- styles -->
+    <link href="{!! asset('css/bootstrap.min.css') !!}" media="all" rel="stylesheet" type="text/css" />
+
+    <link href="{!! asset('css/toolkit.css') !!}" media="all" rel="stylesheet" type="text/css" />
+
+    <link href="{!! asset('css/application.css') !!}" media="all" rel="stylesheet" type="text/css" />
+
+
+<!--[if lt IE 9]>
+<script src="//html5shiv.googlecode.com/svn/trunk/html5.js" type="text/javascript"></script>
+<![endif]-->
+
+</head>
+
+<body id="hey-everyone" class="swag-line template-page" >
+
+  <div class="container">
+    <nav class="navbar navbar-default navbar-tall navbar-full" role="navigation">
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#global-nav">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="/">Bootstrap Themes</a>
+  </div>
+
+  <div class="collapse navbar-collapse" id="global-nav">
+    <div class="navbar-right">
+      <ul class="nav navbar-nav">
+        
+          <li>
+            <a href="{{url('chops')}}">Home</a>
+          </li>
+
+          <li>
+              <a href="{{url('terms')}}">Terms</a>
+          </li>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
+
+    
+
+<h1 class="rte-heading text-center">Hey everyone!</h1>
+<div class="row">
+  <div class="rte col-md-offset-2 col-md-8 col-sm-12 col-sm-offset-0">
+    <h4 name="60df" class="graf--p">Introducing Official Bootstrap themes from the founders of Bootstrap.</h4>
+<p name="d493" class="graf--p">In the last four years we’ve spent thousands of hours designing and developing Bootstrap and other open source projects for millions of people and sites across the web. In that time we’ve also helped to build some amazing products like Twitter, GitHub, Medium, and more. Now, we’re hoping to do it with our own business by creating and selling high quality Bootstrap themes.</p>
+<p name="d9d4" class="graf--p">To start, we’re launching with three themes, one for each of the most popular types of Bootstrapped sites — dashboards, applications, and marketing sites. Each theme includes:</p>
+<ul class="postList">
+<li name="d762" class="graf--li">Beautiful example pages</li>
+<li name="8ffb" class="graf--li">Custom documentation</li>
+<li name="daaf" class="graf--li">Several brand new, custom components</li>
+<li name="e5dc" class="graf--li">Source Less and JavaScript files</li>
+<li name="bece" class="graf--li">Easy build tools with Gulp</li>
+</ul>
+<p name="243f" class="graf--p">We’ve designed our themes as toolkits, just like Bootstrap, to provide you with a highly functional, visually consistent, and beautiful set of components.</p>
+<p name="6389" class="graf--p">While Bootstrap is free for everyone, these themes are only available for purchase. We decided to sell them for a few reasons:</p>
+<ul class="postList">
+<li name="89a7" class="graf--li">We think they’re worth it. Each of us has years of experience building for the web — from products to frameworks — and we’ve poured all of that into these themes.</li>
+<li name="81fc" class="graf--li">We keep Bootstrap funded and available for everyone. To date, we have never taken money from investors and have no corporate sponsorships. All our expenses are paid for by us through ad revenue on our docs.</li>
+<li name="1f3d" class="graf--li">We can give more back to the Bootstrap and open source communities with event sponsorships, swag, and more open source projects.</li>
+</ul>
+<p name="857d" class="graf--p">Bootstrap’s entire mission is to help awesome people do awesome stuff. It’s been our goal since day one and we’re excited to take that to another level with Bootstrap Themes.</p>
+<p name="1794" class="graf--p">❤,<br /><a href="https://github.com/mdo" data-href="https://github.com/mdo" class="markup--anchor markup--p-anchor">@mdo</a>, <a href="https://github.com/fat" data-href="https://github.com/fat" class="markup--anchor markup--p-anchor">@fat</a>, and <a href="https://github.com/dhg" data-href="https://github.com/dhg" class="markup--anchor markup--p-anchor">@dhg</a></p>
+<p> </p>
+  </div>
+</div>
+
+  </div>
+
+  <footer class="footer clearfix">
+  <ul class="footer-social-buttons">
+    <li class="btn-follow hidden-xs">
+      <a href="https://twitter.com/getbootstrap" class="twitter-follow-button" data-link-color="#0069D6" data-show-count="true">Follow @getbootstrap</a>
+    </li>
+    <li class="btn-tweet">
+      <a href="https://twitter.com/share" class="twitter-share-button" data-url="http://themes.getbootstrap.com/" data-count="horizontal" data-via="getbootstrap" data-related="getbootstrap,mdo,fat,dhg">Tweet</a>
+    </li>
+  </ul>
+
+  <p class="hidden-xs">
+    Designed and built with all the love in the world by
+    <a href="https://twitter.com/mdo" target="_blank">@mdo</a>,
+    <a href="https://twitter.com/fat" target="_blank">@fat</a>, and
+    <a href="https://twitter.com/dhg" target="_blank">@dhg</a>.
+  </p>
+
+  <ul class="footer-list">
+    
+      <li class="footer-list-item">
+        <a class="footer-list-link" href="/pages/about-us">About</a>
+      </li>
+    
+      <li class="footer-list-item">
+        <a class="footer-list-link" href="/pages/terms-of-service">Terms</a>
+      </li>
+    
+      <li class="footer-list-item">
+        <a class="footer-list-link" href="/pages/privacy-policy">Privacy</a>
+      </li>
+    
+      <li class="footer-list-item">
+        <a class="footer-list-link" href="/pages/our-license">License</a>
+      </li>
+    
+      <li class="footer-list-item">
+        <a class="footer-list-link" href="/pages/faq">FAQ</a>
+      </li>
+    
+  </ul>
+</footer>
+
+  <!-- Scripts -->
+  <script src="{!! asset('js/jquery.js') !!}"></script>
+
+
+  <!-- Bootstrap Core JavaScript -->
+  <script src="{!! asset('js/bootstrap.min.js') !!}"></script>
+
+  <script src="{!! asset('js/chart.js') !!}"></script>
+
+  <script src="{!! asset('js/toolkit.js') !!}"></script>
+
+  <script src="{!! asset('js/application.js') !!}"></script>
+
+</body>
+</html>
---
+++ resources/views/terms.blade.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<!-- saved from url=(0053)http://themes.getbootstrap.com/pages/terms-of-service -->
+<html class="js"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <!-- General Head Content -->
+  <!-- meta tags -->
+<meta charset="utf-8">
+<!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1' /><![endif]-->
+
+<meta name="description" content="We try hard to keep the legal stuff simple. 1. General Terms 1.1) By accessing and placing an order with Bootstrap Themes, you confirm that you are in agreement">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="google-site-verification" content="4--Yv8-XVyrl4hAM2ceeBXt8H8iiI_3Ms3vDSIV5xN0">
+
+
+<!-- title tag-->
+<title>
+  Terms of Service
+</title>
+
+<!-- search engine -->
+<link rel="canonical" href="./Terms of Service_files/Terms of Service.html">
+
+<!-- open graph bullshit -->
+
+    
+  <meta property="og:title" content="Terms of Service">
+  <meta property="og:type" content="website">
+
+
+
+<meta property="og:description" content="We try hard to keep the legal stuff simple. 1. General Terms 1.1) By accessing and placing an order with Bootstrap Themes, you confirm that you are in agreement">
+
+<meta property="og:url" content="http://themes.getbootstrap.com/pages/terms-of-service">
+<meta property="og:site_name" content="Bootstrap Themes">
+
+
+
+
+    <!-- styles -->
+    <link href="{!! asset('css/bootstrap.min.css') !!}" media="all" rel="stylesheet" type="text/css" />
+
+    <link href="{!! asset('css/toolkit.css') !!}" media="all" rel="stylesheet" type="text/css" />
+
+    <link href="{!! asset('css/application.css') !!}" media="all" rel="stylesheet" type="text/css" />
+
+<!--[if lt IE 9]>
+<script src="//html5shiv.googlecode.com/svn/trunk/html5.js" type="text/javascript"></script>
+<![endif]-->
+
+</head>
+
+<body id="terms-of-service" class="swag-line template-page">
+
+  <div class="container">
+    <nav class="navbar navbar-default navbar-tall navbar-full" role="navigation">
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#global-nav">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="http://themes.getbootstrap.com/">Bootstrap Themes</a>
+  </div>
+
+  <div class="collapse navbar-collapse" id="global-nav">
+    <div class="navbar-right">
+      <ul class="nav navbar-nav">
+        
+          <li>
+              <a href="{{url('chops')}}">Home</a>
+          </li>
+        
+          <li>
+              <a href="{{url('about')}}">About</a>
+          </li>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
+
+    
+
+<h1 class="rte-heading text-center">Terms of Service</h1>
+<div class="row">
+  <div class="rte col-md-offset-2 col-md-8 col-sm-12 col-sm-offset-0">
+    <h4><span class="s1">We&nbsp;try hard to keep the legal stuff simple.</span></h4>
+<h2>1. General Terms</h2>
+<p>1.1) By accessing and placing an order with Bootstrap Themes, you confirm that you are in agreement with and bound by the terms and conditions contained in the Terms Of Use outlined below. These terms apply to the entire website and any email or other type of communication between you and Bootstrap Themes.</p>
+<h2>2. Products</h2>
+<p>2.1) All products and services are delivered by Bootstrap themes&nbsp;electronically to your email address.</p>
+<p>2.2)&nbsp;Bootstrap Themes is not responsible for any technological delays beyond our control. If your spam blocker blocks our emails from reaching you or you do not provide a valid email address where you can be reachable then you can access your download from the Purchases page.</p>
+<h2>3. Security</h2>
+<p>3.1) Bootstrap Themes&nbsp;does not process any order payments through the website. All payments are processed securely through PayPal, a third party online payment provider. Feel free to contact us about our security policies.</p>
+<h2>4. Refunds</h2>
+<p>4.1) You have 24 hours to inspect your purchase and to determine if it does not meet with the expectations laid forth by the seller. In the event that you wish to receive a refund, Bootstrap Themes&nbsp;will issue you a refund and ask you to specify how the product failed to live up to expectations.</p>
+<h2>5. Ownership</h2>
+<p>5.1) Ownership of the product is governed by the usage license selected by the seller.</p>
+<h2>Changes to terms</h2>
+<p>If we change our terms of use we will post those changes on this page. Registered users will be sent an email that outlines changes made to the terms of use.</p>
+  </div>
+</div>
+
+  </div>
+
+  <footer class="footer clearfix">
+  <ul class="footer-social-buttons">
+    <li class="btn-follow hidden-xs">
+      <iframe id="twitter-widget-i1440621625516775496" scrolling="no" frameborder="0" allowtransparency="true" class="twitter-follow-button twitter-follow-button-rendered" src="./Terms of Service_files/follow_button.8a755018e6a97b17947c10de5373fa76.en.html" style="position: static; visibility: visible; width: 224px; height: 20px;" data-screen-name="getbootstrap"></iframe>
+    </li>
+    <li class="btn-tweet">
+      <iframe id="twitter-widget-i1440621625519306027" scrolling="no" frameborder="0" allowtransparency="true" class="twitter-share-button twitter-share-button-rendered twitter-tweet-button" src="./Terms of Service_files/tweet_button.77d41fbc5fa275535329b16ae6381b57.en.html" style="position: static; visibility: visible; width: 97px; height: 20px;" data-url="http://themes.getbootstrap.com/"></iframe>
+    </li>
+  </ul>
+
+  <p class="hidden-xs">
+    Designed and built with all the love in the world by
+    <a href="https://twitter.com/mdo" target="_blank">@mdo</a>,
+    <a href="https://twitter.com/fat" target="_blank">@fat</a>, and
+    <a href="https://twitter.com/dhg" target="_blank">@dhg</a>.
+  </p>
+
+  <ul class="footer-list">
+    
+      <li class="footer-list-item">
+        <a class="footer-list-link" href="http://themes.getbootstrap.com/pages/about-us">About</a>
+      </li>
+    
+      <li class="footer-list-item">
+        <a class="footer-list-link" href="./Terms of Service_files/Terms of Service.html">Terms</a>
+      </li>
+    
+      <li class="footer-list-item">
+        <a class="footer-list-link" href="http://themes.getbootstrap.com/pages/privacy-policy">Privacy</a>
+      </li>
+    
+      <li class="footer-list-item">
+        <a class="footer-list-link" href="http://themes.getbootstrap.com/pages/our-license">License</a>
+      </li>
+    
+      <li class="footer-list-item">
+        <a class="footer-list-link" href="http://themes.getbootstrap.com/pages/faq">FAQ</a>
+      </li>
+    
+  </ul>
+</footer>
+
+
+  <!-- General JavaScript -->
+  <!-- jquery.js -->
+  <script src="{!! asset('js/jquery.js') !!}"></script>
+
+<!-- bootstrap.js -->
+  <script src="{!! asset('js/bootstrap.min.js') !!}"></script>
+
+<!-- Cart.js -->
+  <script src="{!! asset('js/chart.js') !!}"></script>
+
+<!-- Our main app.js -->
+  <script src="{!! asset('js/toolkit.js') !!}"></script>
+
+  <script src="{!! asset('js/application.js') !!}"></script>
+
+</body>
+</html>
