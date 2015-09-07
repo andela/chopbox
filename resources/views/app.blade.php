3fda6d0a85efc38f2801b0829f88fd6df967596b
andela-kerinoso <kolawole.erinoso@andela.com>
2015-09-07 17:19:38 +0000

    Redesign landing page

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,14 +5,12 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>ChopBox | @yield('title')</title>
-<link href="{!! asset('css/bootstrap.min.css') !!}" media="all"
+<link href="{!! asset('css/bootstrap.css') !!}" media="all"
   rel="stylesheet" type="text/css" />
-
 <!-- Custom CSS -->
-<link href="{!! asset('css/landing-page.css') !!}" rel="stylesheet">
-    <link href="{!! asset('css/forms.css') !!}" rel="stylesheet" type="text/css">
-    <link href="{!! asset('css/dropzone.css') !!}" rel="stylesheet" type="text/css" media="all">
-
+    <link href="{!! asset('css/social.min.css') !!}" rel="stylesheet">
+    <link rel="stylesheet" type="text/css"  href="http://x.tagstat.com/dyn/css/0/ax8U-nqOs.css"></link>
+    <link href="{!! asset('css/landing-page.css') !!}" rel="stylesheet">
 <!-- Custom Fonts -->
 <link href="{!! asset('font-awesome/css/font-awesome.min.css') !!}" rel="stylesheet" type="text/css">
 
