283ea07402afdd47804003ea6e34ae3f8cb83213
andela-ooduye <oduyeoluwayemisi@andela.co>
2015-09-23 23:01:46 +0100

    Added the authorization service for chops update display

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -27,5 +27,8 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies($gate);
 
         //
+        $gate->define('update-chop', function ($user, $chop) {
+            return $user->id === $chop->user_id;
+        });
     }
 }
