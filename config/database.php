a623c03d1ee6ccac8ce0f96e4be5b2485227f612
busayo <prosperotemuyiwa@gmail.com>
2015-10-12 01:51:02 +0100

    Change default database connection to pgsql and remove schema key attribute in pgsql connection

--- config/database.php
+++ config/database.php
@@ -23,7 +23,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -81,8 +81,7 @@ return [
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
-            'prefix' => env('DB_PREFIX', ''),
-            'schema' => 'public'
+            'prefix' => env('DB_PREFIX', '')
         ],
 
         'sqlsrv' => [
