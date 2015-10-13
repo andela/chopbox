3d7b65de4dfa488cda652bb0eb05c5e0a8eddb68
busayo <prosperotemuyiwa@gmail.com>
2015-10-13 04:16:53 +0100

    [Chopbox][Chore] Add css fixes and docblocks

--- app/Chopbox/Repository/UserRepository.php
+++ app/Chopbox/Repository/UserRepository.php
@@ -6,11 +6,20 @@ use ChopBox\User;
 
 class UserRepository
 {
+    /**
+     * Find Users by their Emails
+     * @param  string $email
+     * @return Collection
+     */
     public function findUserByEmail($email)
     {
         return $user = User::where('email', '=', $email)->first();
     }
 
+    /**
+     * Find top 10 Users that have posted the most chops
+     * @return [type] [description]
+     */
     public function topUsers()
     {
         return User::orderBy('chops_count', 'DESC')->take(10)->get();
