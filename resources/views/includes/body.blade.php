2a045a0ea05316c5276126b019ff3783e458e15f
busayo <prosperotemuyiwa@gmail.com>
2015-10-14 10:36:40 +0100

    Revert "Merge pull request #72 from andela/hotfix/landing-navbar"
    This reverts commit 6c9e08cb0e4568ba2633251ebff5257d8a62e859, reversing
    changes made to 4fa08868961794b75130e34e888ce356d3d278fe.

--- resources/views/includes/body.blade.php
+++ resources/views/includes/body.blade.php
@@ -1,45 +1,23 @@
 {{--Nav bar--}}
-@if ( Auth::check() )
-    <div class="anp" id="app-growl"></div>
-    <nav class="ck pd ot app-navbar">
-        <div class="by">
-            <div class="os">
-                <button type="button" class="ov collapsed" data-toggle="collapse" data-target="#navbar-collapse-main">
-                    <span class="cv">Toggle navigation</span>
-                    <span class="ow"></span>
-                    <span class="ow"></span>
-                    <span class="ow"></span>
-                </button>
-                <a class="chopbox navbar-brand" href="{{url('/')}}">
-                    ChopBox
-                </a>
-            </div>
+<div class="anp" id="app-growl"></div>
+<nav class="ck pd ot app-navbar">
+  <div class="by">
+    <div class="os">
+      <button type="button" class="ov collapsed" data-toggle="collapse" data-target="#navbar-collapse-main">
+        <span class="cv">Toggle navigation</span>
+        <span class="ow"></span>
+        <span class="ow"></span>
+        <span class="ow"></span>
+      </button>
+      <a class="chopbox navbar-brand" href="{{url('/')}}">
+        ChopBox
+      </a>
+    </div>
 
-            @yield('navbar')
+      @yield('navbar')
 
-        </div>
-    </nav>
-@else
-    <div class="anp" id="app-growl"></div>
-    <nav class="ck pd ot landing-navbar">
-        <div class="by">
-            <div class="os">
-                <button type="button" class="ov collapsed" data-toggle="collapse" data-target="#navbar-collapse-main">
-                    <span class="cv">Toggle navigation</span>
-                    <span class="ow"></span>
-                    <span class="ow"></span>
-                    <span class="ow"></span>
-                </button>
-                <a class="chopbox navbar-brand landing-navbar-brand" href="{{url('/')}}">
-                    ChopBox
-                </a>
-            </div>
-
-            @yield('navbar')
-
-        </div>
-    </nav>
-@endif
+  </div>
+</nav>
 
 {{--Content--}}
 @yield('content')
